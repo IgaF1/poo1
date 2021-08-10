4b3bb8e08f5af27f30003cf73ca493a346e6ebff
59399 <59399@DESKTOP-38L1KF2>
2021-08-10 10:07:37 -0500

    Trabajo en clase 1

---
+++ src/ec/ecu/intsuperior/modelo/Clase_A.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.ecu.intsuperior.modelo;
+
+/**
+ *
+ * @author 59399
+ */
+public class Clase_A {
+    
+    public static void caminar(){
+        System.out.println("Hola yo soy el metodo para caminar");
+        
+    }
+    public static String retornarDatos(){
+        String x;
+        x="Hola";
+        return x;
+      }    
+     
+    public void vacilar(){
+        System.out.println("Hoy no estudio POO por que me voy de vacile");
+    }
+  
+    public boolean estudiar(){
+        
+        return 5==6;//false
+        
+    }        
+}
---
+++ src/ec/ecu/intsuperior/vista/Aplicacion.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.ecu.intsuperior.vista;
+
+import ec.ecu.intsuperior.modelo.Clase_A;
+
+/**
+ *
+ * @author 59399
+ */
+public class Aplicacion {
+    
+    public static void main(String[] args) {
+        
+        Clase_A obj = new Clase_A();
+        System.out.println(obj.estudiar());
+        
+        
+    }
+    
+    
+    
+}
